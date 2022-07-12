a81a79ce0738df4435483f3c47eda73568621a55
Alex Birtwell <alex@alexbirtwell.co.uk>
2022-07-12 10:44:43 +0100

    default schema to allow migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\ServiceLog;
 use App\Models\ServiceNote;
 use App\Observers\ServiceLogObserver;
 use App\Observers\ServiceNoteObserver;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+         Schema::defaultStringLength(191);
          ServiceLog::observe(ServiceLogObserver::class);
          ServiceNote::observe(ServiceNoteObserver::class);
     }
