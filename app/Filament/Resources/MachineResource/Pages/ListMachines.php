c553a2b3efab08d0ded5d155824efd54884d8eef
Alex Birtwell <alex@alexbirtwell.co.uk>
2024-01-16 19:36:38 +0000

    add export for machines

--- app/Filament/Resources/MachineResource/Pages/ListMachines.php
+++ app/Filament/Resources/MachineResource/Pages/ListMachines.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\MachineResource\Pages;
 use App\Filament\Resources\MachineResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListMachines extends ListRecords
 {
@@ -14,6 +16,10 @@ class ListMachines extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+             ExportAction::make()
+                ->exports([
+                    ExcelExport::make('table')->fromTable(),
+                ]),
         ];
     }
 }
