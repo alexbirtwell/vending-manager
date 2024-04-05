cb4848ec451ee6862167c67878f48cb13d90b58e
Alex Birtwell <alex@alexbirtwell.co.uk>
2024-04-05 08:33:07 +0100

    password creation

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -17,16 +17,6 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\Action::make('send_reset_link')
-                        ->label('Send Password Reset Link')
-                        ->action(action: function (?User $record) {
-                            Password::sendResetLink(['email' => $record?->email]);
-                            Notification::make('reset')
-                                ->success()
-                                ->body('Password reset link sent.')
-                                ->send();
-                        })
-                        ->icon('heroicon-o-paper-airplane'),
             Actions\DeleteAction::make(),
         ];
     }
