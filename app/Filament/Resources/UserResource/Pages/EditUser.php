f51a10c276db1b5638676e446d6e2dd14fa2a62b
Alex Birtwell <alex@alexbirtwell.co.uk>
2024-04-05 08:18:02 +0100

    password creation

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Notifications\Notification;
+use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Password;
 
 class EditUser extends EditRecord
 {
@@ -13,6 +17,16 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('send_reset_link')
+                        ->label('Send Password Reset Link')
+                        ->action(action: function (?User $record) {
+                            Password::sendResetLink(['email' => $record?->email]);
+                            Notification::make('reset')
+                                ->success()
+                                ->body('Password reset link sent.')
+                                ->send();
+                        })
+                        ->icon('heroicon-o-paper-airplane'),
             Actions\DeleteAction::make(),
         ];
     }
